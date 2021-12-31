21f668a85ae2df310f77efecf8332ea9c5bbfc88
wurifah <95538965+wurifah@users.noreply.github.com>
2021-12-31 21:08:58 +0700

    Create project.py
    masih kurang input receiver

---
+++ project.py
@@ -0,0 +1,35 @@
+# Imports Library smtplib
+import smtplib
+
+# SETUP EMAIL LOGIN 
+gmail_user = input(str("Masukkan akun gmail: "))
+gmail_app_password = input(str("Masukkan password gmail: "))
+
+# SETUP PENERIMA EMAIL
+with open('receiver_list.txt', 'r') as file:
+	penerima = file.read().replace('\n', ',')
+
+# SETUP Pengirim, penerima, judul dan isi email
+sent_from = gmail_user
+sent_subject = input(str("Masukkan  subjek atau judul lalu akhiri dengan enter: "))
+sent_body = input(str("Masukkan pesan yang akan dikirim lalu akhiri dengan enter: "))
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_app_password)
+    server.sendmail(sent_from, sent_to, email_text)
+    server.close()
+
+    print('Email sent!')
+except Exception as exception:
+    print("Error: %s!\n\n" % exception)
